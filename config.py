ac1b7e612644d90e272222f4a022499044011ff7
ZLO#DEV <59707245+g7AzaZLO@users.noreply.github.com>
2024-06-05 19:57:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ bsc_testnet_url = 'https://bsc-testnet-rpc.publicnode.com'
 chain_id = 97
 contract_address = "0x3cC6FC1035465d5b238F04097dF272Fe9b60EB94"
 token_address = "0x06A0F0fa38AE42b7B3C8698e987862AfA58e90D9"
-YOUR_ADDRESS = "0xb69d7F30Ba4CDBcbd6656b4fe3E079e0954D59B3"
+YOUR_ADDRESS = "YOUR_WALLET"
 min_time = 30
 max_time = 450
